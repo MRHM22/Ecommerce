2dd282c80fa738432dcd6b8b202b1e843091aa28
MRHM22 <hdezmartin22@gmail.com>
2025-05-01 19:26:09 -0600

    relaciones db

--- backend/src/Core/Ecommerce.Domain/Category.cs
+++ backend/src/Core/Ecommerce.Domain/Category.cs
@@ -7,4 +7,5 @@ public class Category : BaseDomainModel
 {
     [Column(TypeName = "NVARCHAR(100)")]
     public string? Nombre { get; set; }    
+    public virtual ICollection<Product>? Products { get; set; }
 }
--- backend/src/Core/Ecommerce.Domain/Image.cs
+++ backend/src/Core/Ecommerce.Domain/Image.cs
@@ -9,4 +9,5 @@ public class Image : BaseDomainModel
     public string? Url { get; set; }
     public int ProductId { get; set; }
     public string? PublicCode { get; set; }
+    public virtual Product? Product { get; set; }
 }
